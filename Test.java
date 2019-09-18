8a0d27c751f2abbb76f71136279fb0c30986c2be
Harry Sidhunata <hsidhun@users.noreply.github.com>
2019-09-18 14:30:36 +1000

    Update Test.java

--- Test.java
+++ Test.java
@@ -1,2 +1,7 @@
-This is just a test file
-Some changes on 18/09/2019 1:54 PM
+#This is just a test file
+#Some changes on 18/09/2019 1:54 PM
+public class Test {
+
+  public static void main(String[] args) {
+  }
+}
